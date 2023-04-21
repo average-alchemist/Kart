e9a7c807316c69f0444cc117fcb11702016b5f58
Karić Kenan <kenan.karic@outlook.com>
2023-04-21 20:00:58 +0200

    feat: Provide Ktor client module

--- app/src/main/java/io/aethibo/kart/core/network/NetworkModule.kt
+++ app/src/main/java/io/aethibo/kart/core/network/NetworkModule.kt
@@ -1,77 +1,58 @@
 package io.aethibo.kart.core.network
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.aethibo.kart.BuildConfig
-import kotlinx.serialization.ExperimentalSerializationApi
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.accept
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import okhttp3.Interceptor
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-   private const val INTERCEPTOR_LOGGING_NAME = "INTERCEPTOR_LOGGING"
-
-   @Provides
-   @Named(INTERCEPTOR_LOGGING_NAME)
-   fun provideHttpLoggingInterceptor(): Interceptor {
-      return if (BuildConfig.DEBUG) {
-         HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-         }
-      } else {
-         noOpInterceptor()
-      }
-   }
-
-   private fun noOpInterceptor(): Interceptor {
-      return Interceptor { chain ->
-         var request = chain.request()
-         request = request.newBuilder()
-            .addHeader("test-header", "Hello, world!")
-            .build()
-
-         chain.proceed(request)
-      }
-   }
 
    @Provides
    @Singleton
-   fun provideOkHttpClient(
-      @Named(INTERCEPTOR_LOGGING_NAME) loggingInterceptor: Interceptor
-   ): OkHttpClient {
-      return OkHttpClient
-         .Builder()
-         .apply {
-            addNetworkInterceptor(loggingInterceptor)
+   fun provideKtorClient(): HttpClient {
+      return HttpClient(Android) {
+         defaultRequest {
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+            url(BuildConfig.DUMMY_API_URL)
+            header("X-Custom-Header", "Hello, world!")
          }
-         .build()
-   }
-
-   @OptIn(ExperimentalSerializationApi::class)
-   @Provides
-   @Singleton
-   fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
-      val json = Json {
-         ignoreUnknownKeys = true
-         coerceInputValues = true
+         install(HttpTimeout) {
+            socketTimeoutMillis = 30_000
+            requestTimeoutMillis = 30_000
+            connectTimeoutMillis = 30_000
+         }
+         install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.BODY
+         }
+         install(ContentNegotiation) {
+            json(Json {
+               prettyPrint = true
+               isLenient = true
+               ignoreUnknownKeys = true
+               encodeDefaults = true
+            })
+         }
       }
-      val contentType = "application/json".toMediaType()
-
-      return Retrofit
-         .Builder()
-         .addConverterFactory(json.asConverterFactory(contentType))
-         .baseUrl(BuildConfig.DUMMY_API_URL)
-         .client(okHttpClient)
-         .build()
    }
 }
