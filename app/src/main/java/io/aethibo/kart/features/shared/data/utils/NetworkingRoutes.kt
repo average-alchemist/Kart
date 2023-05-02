ace2560e5c2bb6bb35a9a076c02da3d23095579f
Karić Kenan <kenan.karic@outlook.com>
2023-05-02 17:08:25 +0200

    feat: Update API fetch

--- app/src/main/java/io/aethibo/kart/features/shared/data/utils/NetworkingRoutes.kt
+++ app/src/main/java/io/aethibo/kart/features/shared/data/utils/NetworkingRoutes.kt
@@ -1,6 +1,6 @@
 package io.aethibo.kart.features.shared.data.utils
 
 object NetworkingRoutes {
-   const val PRODUCTS = "products"
+   const val PRODUCTS = "products/category/furniture"
    const val ADD_PRODUCT = "${PRODUCTS}/add"
 }
